7e447b10b411ffab471830cd2fe59d07121f5de1
supermantoman <jwg7632@gmail.com>
2023-02-27 21:25:47 +0900

    유니온타입 연습

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let MemberA = { member1: 'kkk', member2: 'd' };
 let MemberB = { member2: 'hi' };
 let game = ['12323', 1123, true]; //타입 자동완성
 let named = 'Jeon';
-let age = 20;
+// let age = 20;
 let whereFrom = 'Daegu';
 let favorites = {
     song: '호랑이소굴',
@@ -15,3 +15,17 @@ let project = {
     days: 30,
     started: true,
 };
+let HH = [1, '2', 3]; //배열에 숫자형, 문자형 타입 모두 들어오고 싶을 때
+let Obj = {
+    a: 123
+}; // 객체의 속성값에 여러가지 타입 값이 올 수 있도록 하고 싶을 때
+let Most;
+Most = 123;
+Most = '123';
+Most = 123;
+let agee = 1;
+agee + 1;
+let user = 'kim';
+let age = undefined;
+let married = false;
+let 철수 = [user, age, married];
--- index.ts
+++ index.ts
@@ -12,14 +12,14 @@ let game = ['12323',1123,true]; //타입 자동완성
 
 
 let named = 'Jeon';
-let age = 20;
+// let age = 20;
 let whereFrom = 'Daegu';
 let favorites:{song:string, singer:string} = {
   song: '호랑이소굴',
   singer: 'Jvcki Wai',
 }
 
-// type ProjectType = {member:[string,string], days:number,started:boolean};
+// type ProjectType = {member:string[], days:number,started:boolean};
 type ProjectType = {[key:string]:string[]|number|boolean};
 
 let project:ProjectType = {
@@ -27,3 +27,21 @@ let project:ProjectType = {
   days : 30,
   started : true,
 }
+
+let HH :(number|string)[] = [1,'2',3]; //배열에 숫자형, 문자형 타입 모두 들어오고 싶을 때
+let Obj :{a:number|string|boolean} = {
+  a:123
+}; // 객체의 속성값에 여러가지 타입 값이 올 수 있도록 하고 싶을 때
+
+let Most:unknown;
+Most = 123;
+Most = '123';
+Most = 123;
+
+let agee:unknown = 1;
+agee + 1
+
+let user:string = 'kim';
+let age:undefined|number= undefined;
+let married:boolean = false; 
+let 철수:(string|undefined|boolean)[] = [user, age, married];
