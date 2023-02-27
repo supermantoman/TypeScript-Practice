c0da544d2cad3e83f2dda5cc3d8f7e824a0f298c
supermantoman <jwg7632@gmail.com>
2023-02-27 20:48:06 +0900

    타입변수를 활용한 반복가능한 객체 구성

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ let Practice = [1, 323, '4242', true, false, {}];
 let MemberList = { member1: 'John', member2: 'Amy' };
 let MemberA = { member1: 'kkk', member2: 'd' };
 let MemberB = { member2: 'hi' };
+let game = ['12323', 1123, true]; //타입 자동완성
+let named = 'Jeon';
+let age = 20;
+let whereFrom = 'Daegu';
+let favorites = {
+    song: '호랑이소굴',
+    singer: 'Jvcki Wai',
+};
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -7,3 +7,23 @@ let Practice :HyperType[] = [1,323,'4242',true,false,{}];
 let MemberList : {member1: string, member2: string} = {member1 : 'John', member2:'Amy'};
 let MemberA : {member1?: string, member2?: string} = {member1:'kkk', member2: 'd'};
 let MemberB : {member1?: string, member2?: string} = {member2:'hi'};
+
+let game = ['12323',1123,true]; //타입 자동완성
+
+
+let named = 'Jeon';
+let age = 20;
+let whereFrom = 'Daegu';
+let favorites:{song:string, singer:string} = {
+  song: '호랑이소굴',
+  singer: 'Jvcki Wai',
+}
+
+// type ProjectType = {member:[string,string], days:number,started:boolean};
+type ProjectType = {[key:string]:string[]|number|boolean};
+
+let project:ProjectType = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}
