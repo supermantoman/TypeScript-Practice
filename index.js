fd9f0161bdccd410e735a66d0b4f58bd713d2007
supermantoman <jwg7632@gmail.com>
2023-02-27 20:32:58 +0900

    객체 타입지정 연습, ? 옵션 연습

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 let Practice = [1, 323, '4242', true, false, {}];
+let MemberList = { member1: 'John', member2: 'Amy' };
+let MemberA = { member1: 'kkk', member2: 'd' };
+let MemberB = { member2: 'hi' };
--- index.ts
+++ index.ts
@@ -2,3 +2,8 @@ type MyType = string|number;
 type HyperType = string|number|boolean|[]|{};
 
 let Practice :HyperType[] = [1,323,'4242',true,false,{}];
+
+
+let MemberList : {member1: string, member2: string} = {member1 : 'John', member2:'Amy'};
+let MemberA : {member1?: string, member2?: string} = {member1:'kkk', member2: 'd'};
+let MemberB : {member1?: string, member2?: string} = {member2:'hi'};
